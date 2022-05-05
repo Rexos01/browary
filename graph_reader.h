0c5279e0b5d1f63a5254253edfee84d95e321ed4
Dominika <sluzynskadominika@gmail.com>
2022-05-05 14:33:45 +0200

    graph reader + addSource

--- graph_reader.h
+++ graph_reader.h
@@ -3,16 +3,14 @@
 
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <fstream>
-#include <string>
-#include <list>
-#include <algorithm> 
+
 
 class GraphReader
 {
     static std::vector<std::vector<int>> readToMatrix(std::string path);
     static std::vector<std::vector<int>> readToList(std::string path);
+    static std::vector<std::vector<int>> addSource (std::vector<std::vector<int>> matrix, std::string path, char z, char u);
 };
 
 #endif
