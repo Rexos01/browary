74c742c8d38859a0f47e771f46e2973fef4d79c5
Zuzanna-K <104144931+Zuzanna-K@users.noreply.github.com>
2022-05-30 22:40:27 +0200

    Update graph_reader.h

--- graph_reader.h
+++ graph_reader.h
@@ -7,15 +7,23 @@
 #include <string>
 #include <list>
 #include <algorithm>
-#include <tuple>
+
+struct EdgeData
+{
+    int flow;
+    int capacity;
+    int cost;
+};
 
 class GraphReader
 {
 public:
     static std::vector<std::vector<int>> readToMatrix(std::string path);
+    static std::vector<std::vector<EdgeData>> readToMatrixOfCosts(std::string path);
     static std::vector<std::vector<int>> readToList(std::string path);
-    static std::vector<std::tuple<int,int, int>> readAsWeightedList(std::string path);
     static std::vector<std::vector<int>> addSource (std::vector<std::vector<int>> matrix, std::string path, char z, char u);
+    static std::vector<std::vector<EdgeData>> addSourceToMatrixOfCosts(std::vector<std::vector<EdgeData>> matrix, std::string path, char z, char u);
+
 };
 
 #endif
