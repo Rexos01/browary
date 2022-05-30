f096058ca93b71841520f36b7b1e5e493b699c32
Zuzanna-K <104144931+Zuzanna-K@users.noreply.github.com>
2022-05-30 22:37:49 +0200

    Update graph.h

--- graph.h
+++ graph.h
@@ -4,25 +4,42 @@
 #include <vector>
 #include <algorithm>
 #include <queue>
-#include <tuple>
+#include "graph_reader.h"
+
+struct data 
+{
+  int distance;
+  int previous;
+};
+
+/*
+struct edgeData
+{
+    int flow;
+    int capacity;
+    int cost;
+};
+*/
+
 class Graph
 {
 private:
     std::vector<std::vector<int>> matrix;
+    std::vector<std::vector<EdgeData>> matrixOfCosts;
 
 public:
-    std::vector<std::tuple<int,int,int>> neighborsList;
     Graph(std::vector<std::vector<int>> incidMatrix);
-    Graph(std::vector<std::tuple<int,int,int>> list);
-    int MaxFlow(int start, int end, std::vector<int> &output, std::vector<std::vector<int>> &outputResidual);
-    bool BFS(int start, int end, std::vector<int> &path);
+    int MaxFlow(int start, int end, std::vector<int>& output);
+    bool BFS(int start, int end, std::vector<int>& path);
     bool BFS(std::vector<std::vector<int>> graph, int start, int finish, std::vector<int> &path);
     void PrintGraph();
-    void PrintEdges();
     void UpdateMatrix(std::vector<std::vector<int>> matrix);
     void UpdateMatrixRow(std::vector<int> column, int index);
     std::vector<std::vector<int>> GetMatrix();
-    std::vector<int> BellmanFord(std::vector<std::tuple<int,int,int>> neigboorsList, int source);
+
+    void UpdateMatrixofcosts(std::vector<std::vector<EdgeData>> matrix);
+    void PrintMatrixOfCosts();
+    std::vector<data> BellmanFord(std::vector<std::vector<EdgeData>> matrix, int start);  
 };
 
 #endif
