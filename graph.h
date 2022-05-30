17901a75d3cdb4a86f377e8a93ed53af3794bfaa
Zuzanna-K <104144931+Zuzanna-K@users.noreply.github.com>
2022-05-30 22:44:13 +0200

    Update graph.h

--- graph.h
+++ graph.h
@@ -29,7 +29,7 @@ private:
 
 public:
     Graph(std::vector<std::vector<int>> incidMatrix);
-    int MaxFlow(int start, int end, std::vector<int>& output);
+    int MaxFlow(int start, int end, std::vector<int> &output, std::vector<std::vector<int>> &outputResidual);
     bool BFS(int start, int end, std::vector<int>& path);
     bool BFS(std::vector<std::vector<int>> graph, int start, int finish, std::vector<int> &path);
     void PrintGraph();
