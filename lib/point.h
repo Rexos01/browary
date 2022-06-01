d78cacb7d1768fba9aece21cde6bf7292dc8a789
Daniel Bińkowski <daniel1binkowski@gmail.com>
2022-06-01 03:11:44 +0200

    covex set classes docs

--- lib/point.h
+++ lib/point.h
@@ -1,11 +1,26 @@
 #ifndef POINT_H
 #define POINT_H
 
+/**
+ * Klasa punktu geometrycznego
+ */
 struct Point
 {
+  /**
+   * Współrzędna x punktu
+   */
   int x;
+
+  /**
+   * Współrzędna y punktu
+   */
   int y;
 
+  /**
+   * Konstruktor klasy punktu
+   * @param _x współrzędna x położenia
+   * @param _y współrzędna y położenia
+   */
   Point(int _x, int _y);
 
   Point() {}
--- lib/segment.h
+++ lib/segment.h
@@ -3,13 +3,33 @@
 
 #include "point.h"
 
+/**
+ * Klasa reprezentująca odcinek
+ */
 struct Segment
 {
+  /**
+   * Punkt początkowy
+   */
   Point p1;
+
+  /**
+   * Punkt końcowy
+   */
   Point p2;
 
+  /**
+   * Konstruktor klasy odcinka składającego się z dwóch punktów
+   * @param _p1 punkt początkowy Point
+   * @param _p2 punkt końcowy Point
+   */
   Segment(Point _p1, Point _p2);
 
+  /**
+   * Sprawdza, czy dwa odcinki się przecinają
+   * @param s odcinek do sprawdzenia
+   * @return true, jeśli odcinki się przecinają, false w p. w.
+   */
   bool intersects(Segment s);
 };
 
